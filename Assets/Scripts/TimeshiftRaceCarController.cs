cea670ca1dfaa4f5bf62cfb55d86cc59ce0080a3
Emin Karakaya <71080980+eminkarakaya@users.noreply.github.com>
2022-12-15 03:12:32 +0300

    added Boost System

--- Assets/Scripts/TimeshiftRaceCarController.cs
+++ Assets/Scripts/TimeshiftRaceCarController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 public class TimeshiftRaceCarController : MonoBehaviour
 {
+    bool boost;
     Rigidbody rb;
-    [SerializeField] private float maxSpeed,horizontalSpeed,rotationMultiplier;
-    public float speed;
+    [SerializeField] private float maxSpeed,horizontalSpeed,rotationMultiplier,boostedMaxSpeed, speed;
+    [SerializeField] private float accelerationSpeed, decelerationSpeed;
+    float tempMaxSpeed,boostDuration;
     private const string HORIZONTAL = "Horizontal";
     private void Start()
     {
@@ -13,13 +15,41 @@ public class TimeshiftRaceCarController : MonoBehaviour
     }
     private void Update()
     {
+        if (speed < maxSpeed)
+        {
+            speed += Time.deltaTime * accelerationSpeed;
+        }
+        else
+        {
+            if (speed > 0)
+            {
+                speed -= Time.deltaTime * decelerationSpeed;
+            }
+        }
         Vector3 rot = Vector3.forward * Input.GetAxis(HORIZONTAL) * rotationMultiplier;
         transform.rotation = Quaternion.Euler(rot);
         transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis(HORIZONTAL)*horizontalSpeed);
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
-    private void FixedUpdate()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.gameObject.tag == "Speed")
+        {
+            StopAllCoroutines();
+            StartCoroutine(Boost());
+        }
+    }
+    //private void Boost()
+    //{
+    //    tempMaxSpeed = maxSpeed;
+    //    maxSpeed = boostedMaxSpeed;
+    //}
+    private IEnumerator Boost()
+    {
+        tempMaxSpeed = maxSpeed;
+        maxSpeed = boostedMaxSpeed;
+        speed = boostedMaxSpeed;
+        yield return new WaitForSeconds(boostDuration);
+        maxSpeed = tempMaxSpeed;
     }
 }
