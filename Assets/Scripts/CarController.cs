e94384c6342de9d98f7a507dc3d625340d74224b
Emin Karakaya <71080980+eminkarakaya@users.noreply.github.com>
2022-12-19 14:48:45 +0300

    .

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 public class CarController : MonoBehaviour
 {
     public static event System.Action OnFall;
-    private bool _isGrounded;
+    [SerializeField] private bool _isGrounded;
     public bool isGrounded { get => _isGrounded ; set 
         {
             var old = _isGrounded;
@@ -15,6 +16,7 @@ public class CarController : MonoBehaviour
             }
         }
     }
+    [SerializeField] private BoxCollider boxCollider;
     private RaycastHit hit;
     [SerializeField] private float rayLenght;
     [SerializeField] private Transform _backOfCar;
@@ -33,6 +35,9 @@ public class CarController : MonoBehaviour
         ,unTouchableDuration,horizontalSpeed;
     Rigidbody rb;
     public float horizontalData;
+    [SerializeField] private TextMeshProUGUI numberOfText;
+    public int numberOf;
+    Vector3 oldColliderSize;
     private void Awake()
     {
         instance = this;
@@ -40,13 +45,15 @@ public class CarController : MonoBehaviour
     
     private void Start()
     {
+        maxSpeed = data.maxSpeed;
+        oldColliderSize = boxCollider.size;
         levelController = FindObjectOfType<LevelController>();
         meshRenderers = GetComponentsInChildren<MeshRenderer>();
         rb = GetComponent<Rigidbody>();
-        //maxSpeed = data.maxSpeed;
     }
     private void Update()
     {
+        TextParse();
         if(Physics.Raycast(transform.position,Vector3.down,out hit,rayLength,layer))
         {
             if(hit.collider != null)
@@ -63,11 +70,7 @@ public class CarController : MonoBehaviour
         {
             rb.AddForce(Physics.gravity*2,ForceMode.Acceleration);
         }
-
-        HorizontalMove();
-       
-        //rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
-        
+        HorizontalMove();        
         if (rb.velocity.z < maxSpeed)
         {
             rb.velocity += new Vector3(rb.velocity.x, 0, Time.deltaTime * data.accelerationSpeed*2);
@@ -81,20 +84,20 @@ public class CarController : MonoBehaviour
         }
         Vector3 rby = rb.angularVelocity;
         rby.y = 0;
+        Vector3 qwe = transform.rotation.eulerAngles;
+        qwe.y = 0;
+        transform.rotation = Quaternion.Euler(qwe);
 
         rb.angularVelocity = rby;
+        ClampRotation();
     }
     public void HorizontalMove()
     {
-        Vector3 rot = Vector3.forward * rotationMultiplier * (rb.velocity.z / maxSpeed) * horizontalData;
+        Vector3 rot = Vector3.forward * rotationMultiplier * horizontalData;
         Vector3 temp = transform.rotation.eulerAngles;
         temp.z = rot.z;
         transform.rotation = Quaternion.Euler(temp);
-
         rb.velocity = new Vector3(horizontalSpeed * horizontalData, rb.velocity.y,rb.velocity.z);
-        
-
-        //transform.Translate(Vector3.right * Time.deltaTime * data.horizontalSpeed * (rb.velocity.z / maxSpeed) * horizontalData * 1.3f);
     }
     private IEnumerator Revive()
     {
@@ -113,9 +116,7 @@ public class CarController : MonoBehaviour
     private IEnumerator UnTouchable()
     {
         if(!dokunulmazMi)
-        {
-            //rb.constraints = RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationX;
-            
+        {            
             StartCoroutine(MaterialToggle());
             dokunulmazMi = true;
             gameObject.layer = unTouchableLayerIndex;
@@ -126,10 +127,22 @@ public class CarController : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
+        if(other.gameObject.tag == "Wall")
+        {
+            Collider[] colliders = other.gameObject.GetComponentsInChildren<Collider>();
+            foreach (var item in colliders)
+            {
+                Debug.Log(item);
+                item.enabled = true;
+                if(item.TryGetComponent(out Rigidbody rb))
+                {
+                    rb.isKinematic = false;
+                    rb.useGravity = true;
+                }
+            }
+        }
         if(other.gameObject.tag == "Obstacle")
         {
-            //CrashEffect2(other.transform);
-            //CrashEffect();
             StartCoroutine(Revive());
         }
         if(other.gameObject.tag == "Speed")
@@ -138,12 +151,24 @@ public class CarController : MonoBehaviour
         }
         if(other.gameObject.tag =="Finish")
         {
-            OnFinish?.Invoke();
-            isFinish = true;
+            if(TryGetComponent(out PlayerInput playerInput))
+            {
+                OnFinish?.Invoke();
+                isFinish = true;
+            }
+            Finish(other);
         }
         if(other.gameObject.tag == "Object")
         {
             other.GetComponent<Rigidbody>().AddForce((other.transform.position - transform.position).normalized * rb.velocity.z * 3);
+        }
+        if(other.gameObject.tag == "Trap")
+        {
+            WheelBurst();
+        }
+        if(other.gameObject.tag == "Wheel")
+        {
+            FixWheel();
         }
     }
     public void SpecialFinish()
@@ -152,16 +177,6 @@ public class CarController : MonoBehaviour
         {
             specialFinish = false;
         }
-    }
-    void CrashEffect2(Transform target)
-    {
-        rb.AddForce(transform.position - target.position * 100 * rb.velocity.z);
-    }
-    void CrashEffect()
-    {
-        //rb.constraints = RigidbodyConstraints.None;
-        rb.AddExplosionForce(1000 * rb.velocity.z,_backOfCar.position,.3f);
-        
     }
     IEnumerator MaterialToggle()
     {
@@ -217,6 +232,71 @@ public class CarController : MonoBehaviour
 
     void ClampRotation()
     {
-        Debug.Log(transform.rotation + " " + transform.rotation.eulerAngles);
+        if(transform.rotation.eulerAngles.x < 340 && transform.rotation.eulerAngles.x > 180)
+        {
+            transform.rotation = Quaternion.Euler(-20, transform.rotation.y, transform.rotation.z);
+            Vector3 angX = rb.angularVelocity;
+            angX.x = 0;
+            rb.angularVelocity = angX;
+        }
+        else if (transform.rotation.eulerAngles.x > 30 && transform.rotation.eulerAngles.x < 180)
+        {
+            transform.rotation = Quaternion.Euler(25, transform.rotation.y, transform.rotation.z);
+            Vector3 angX = rb.angularVelocity;
+            angX.x = 0;
+            rb.angularVelocity = angX;
+        }
+    }
+    void WheelBurst()
+    {
+        maxSpeed = maxSpeed / 2;
+        Vector3 colliderSize = new Vector3(boxCollider.bounds.size.x, boxCollider.bounds.size.y - .5f, boxCollider.bounds.size.z);
+        boxCollider.size = colliderSize;
+    }
+    void FixWheel()
+    {
+        maxSpeed = data.maxSpeed;
+        boxCollider.size = oldColliderSize;
+    }
+    void Finish(Collider other)
+    {
+        other.GetComponent<MeshRenderer>().enabled = false;
+        other.transform.GetChild(1).GetComponent<MeshRenderer>().enabled = false;
+        other.transform.GetChild(2).GetComponent<MeshRenderer>().enabled = false;
+        other.transform.GetChild(0).gameObject.SetActive(true);
+        
+        other.transform.GetChild(0).GetComponent<Rigidbody>().AddExplosionForce(100, other.transform.GetChild(0).transform.position, 10f);
+
+        //Collider[] colliders = rb.transform.GetChild(0).GetComponentsInChildren<Collider>();
+        //foreach (var item in colliders)
+        //{
+
+        //}
+    }
+    private void OnDrawGizmos()
+    {
+        //Gizmos.DrawSphere()
+    }
+    void TextParse()
+    {
+        if(numberOf+1 == 1)
+        {
+            numberOfText.text = (numberOf + 1) + "st";
+        }
+        else if(numberOf + 1 == 2)
+        {
+            numberOfText.text = (numberOf + 1) + "nd";
+
+        }
+        else if (numberOf + 1 == 3)
+        {
+            numberOfText.text = (numberOf + 1) + "rd";
+
+        }
+        else if (numberOf + 1 > 3)
+        {
+            numberOfText.text = (numberOf + 1) + "th";
+
+        }
     }
 }
